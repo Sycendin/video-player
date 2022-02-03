7708537e75b375e85e02743c1b0fd3769e35ced1
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-02 23:16:46 -0500

    adding finished video player

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector(".player")
 const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
@@ -8,6 +9,8 @@ const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
+const speed = document.querySelector(".player-speed");
+
 // Play & Pause ----------------------------------- //
 const showPlayIcon = () =>{
     playBtn.classList.replace('fa-pause', 'fa-play');
@@ -53,7 +56,7 @@ const setProgress= (e) =>{
     video.currentTime = newTime * video.duration
 }
 // Volume Controls --------------------------- //
-
+let lastVolume = 1;
 //Volume bar
 const changeVolume = (e) =>{
     let volume = e.offsetX / volumeRange.offsetWidth;
@@ -77,15 +80,71 @@ const changeVolume = (e) =>{
     else if (volume === 0) {
         volumeIcon.classList.add('fas', 'fa-volume-off')
     }
+    lastVolume = volume;
 }
 
+//Mute/unmute
+
+const toggleMute = () =>{
+    volumeIcon.className = '';
+    if (video.volume){
+        lastVolume = video.volume;
+        video.volume = 0;
+        volumeBar.style.width = 0;
+        volumeIcon.classList.add('fas', 'fa-volume-mute');
+        volumeIcon.setAttribute('title', 'Unmute');
+        
+    }
+    else{
+        video.volume = lastVolume;
+        volumeBar.style.width = `${lastVolume * 100}%`
+        if (lastVolume > 0.7){
+            volumeIcon.classList.add('fas', 'fa-volume-up');
+        }
+        else{
+            volumeIcon.classList.add('fas', 'fa-volume-down');
+        }
+        
+        volumeIcon.setAttribute('title', 'Mute');
+
+    }
+}
 // Change Playback Speed -------------------- //
 
-
+const changeSpeed = () =>{
+    video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
+/* View in fullscreen */
+function openFullscreen(elem) {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    video.classList.add('video-fullscreen');
+}
 
-
+  /* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+    video.classList.remove('video-fullscreen');
+}
+let fullscreen = false;
+// Toggle fullscreen
+const toggleFullscreen = () =>{
+    !fullscreen ? openFullscreen(player) : closeFullscreen();
+    fullscreen = !fullscreen;
+}
 //Event listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
@@ -93,3 +152,6 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute)
+speed.addEventListener('change', changeSpeed);
+fullscreenBtn.addEventListener('click', toggleFullscreen);
